5dcdc4ceb5d25d91b9a6a86e5736d333bb7a2e32
BlAcK1Dy <122513114+BlAcK1Dy@users.noreply.github.com>
2023-01-15 08:27:35 +0300

    Add files via upload

---
+++ Zadacha/Zadanie1/Zadanie1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string Convert(string word)
+{
+    string new_word = "";
+    transform(word.begin(), word.end(), word.begin(), ::tolower);
+    for (int i = 0; i < word.length(); i++)
+    {
+        if (count(word.begin(), word.end(), word[i]) == 1)
+        {
+            new_word += '(';
+        }
+        else
+        {
+            new_word += ')';
+        }
+    }
+    return new_word;
+}
+
+int main()
+{
+    string str;
+    cout << "Enter word " << endl;
+   
+    cin >> str;
+
+    cout << Convert(str) << endl;
+    
+    system("pause");
+    return 0;
+}
+
